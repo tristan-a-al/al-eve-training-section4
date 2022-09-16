2eea858d6d209512b4dbbf7e20ac5f19149dbb9a
Tristan Ackermann <tristan@al.co.za>
2022-09-16 15:28:51 +0200

    Provided implementation for useInputHook
    Answered Question 6

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -13,8 +13,31 @@ type InputDefinition={
 
 const useInput=(validateValue: (value:any)=>boolean):InputDefinition=>{
 
+    const defaultValue = "";
+    const defaultHasError = false;
+    const defaultIsValid = false;
 
-    return{}
+    const [value, setValue] = useState<string>(defaultValue);
+    const [hasError, setHasError] = useState<boolean>(defaultHasError);
+    const [isValid, setIsValid] = useState<boolean>(defaultIsValid);
+    const inputChangeHandler = ((e: ReactChangeEvent)=> {
+        validateValue(value);
+    });
+    const inputBlurHandler = (() => {
+        validateValue(value);
+    });
+    const reset = () => {
+        setValue(defaultValue);
+        setHasError(defaultHasError);
+        setIsValid(defaultIsValid);
+    } 
+
+
+
+    // if () {
+
+    // }
+    return {value, hasError, inputChangeHandler, inputBlurHandler, reset, isValid};
 
 }
 
