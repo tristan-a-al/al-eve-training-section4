05b3a3489bd5fedb124e9961debf36c2920a5edf
Morgan <50941801+MorganTDicks@users.noreply.github.com>
2022-09-18 17:01:07 +0200

    Question 6 Marked by morgan@al.co.za

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -17,15 +17,38 @@ const useInput=(validateValue: (value:any)=>boolean):InputDefinition=>{
     const defaultHasError = false;
     const defaultIsValid = false;
 
+    //Q6.1 - 2/5
+    // hasError and isValid can both be derived variables
+    // you did not use the Validate value to check  if the value is Valid
+    //no isTouched State?
+
+
+  //Expected similar :
+  //let isValid = validateValue(value)
+  //From the Question 5 we know that an input is invalid (has error)
+  //when the value is invalid and has been touched
+  //let hasError = !isValid && isTouched
+
+
+
     const [value, setValue] = useState<string>(defaultValue);
     const [hasError, setHasError] = useState<boolean>(defaultHasError);
     const [isValid, setIsValid] = useState<boolean>(defaultIsValid);
+    
+    
+    //Q6.2 - 1/2 Read your Question. 
+    //incorrect usage of Validation funtion
+    //Value not updated onChange
+    //isTouched not updated onBlur
+    
     const inputChangeHandler = ((e: ReactChangeEvent)=> {
         validateValue(value);
     });
     const inputBlurHandler = (() => {
         validateValue(value);
     });
+
+    //Q6.3 - 2/2
     const reset = () => {
         setValue(defaultValue);
         setHasError(defaultHasError);
