349fadcd6d37f16ac5a1469e89a3469e48f7ddc7
Morgan <50941801+MorganTDicks@users.noreply.github.com>
2022-09-18 16:42:41 +0200

    Question 6 marked by morgan@al.co.za

--- components/BasicForm/index.tsx
+++ components/BasicForm/index.tsx
@@ -1,14 +1,25 @@
 import styles from "@/styles/Forms.module.css";
 import { ChangeEvent, FormEvent, useState, useEffect } from "react";
 
+
+//Q5.5.1 - 0/1 though you do Disable the button you don't implement proper logic to enable it again.
+//Q5.5.2 - 0/1 You never change your states so you validation is never invalid.
+
 function BasicForm() {
-  // Q5.1
+  // Q5.1 - 2/4  
+  //you are over complicating your types. just string is fine when you set the default to string.
+  // You did not complete the type for the change event. You need to tell it what element to use ChangeEvent<HTMLInputElement>
   const [username, setUsername] = useState<string | number | readonly string[] | undefined>("");
   const [password, setPassword] = useState<string | number | readonly string[] | undefined>("");
-  const [isUsernameTouched, setIsUsernameTouched] = useState<boolean>(false);
-  const [isUsernameValid, setIsUsernameValid] = useState<boolean>(true);
-  const [isPasswordTouched, setIsPasswordTouched] = useState<boolean>(false);
+  
+  //Q 5.2.2 - 4/7  Derived Variables expected.
+  // you incorrectly scoped the isValid constants and no longer satisfies onBlur or on Keystroke validation.
   const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
+  const [isUsernameValid, setIsUsernameValid] = useState<boolean>(true);
+ 
+  //Q5.3.1 - 3/3 good.
+  const [isUsernameTouched, setIsUsernameTouched] = useState<boolean>(false);
+  const [isPasswordTouched, setIsPasswordTouched] = useState<boolean>(false);
   const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(false);
 
   const [pwLabelText, setPWLabelText] = useState<string | null>("Password");
@@ -34,10 +45,10 @@ function BasicForm() {
   }
 
   function submitHandler(event: FormEvent<HTMLFormElement>) {
-    // Q5.2.1 & Q5.4.1
+    // Q5.2.1 
     event.preventDefault();
 
-    // Q5.2.2
+    // Q5.2.2 - incorrectly scoped
     if (username == null || password == null) {
       return;
     }
@@ -47,9 +58,13 @@ function BasicForm() {
 
     console.log(usernameIsValid, passwordIsValid);
 
+    // Q5.4.1 - 1/1 This is incorrectly scoped but will allow the point for correct logic
     const isFormValid = usernameIsValid && passwordIsValid;
 
-    // Q5.2.3
+    // Q5.2.3 - 2/3  to do nothing in a function you need to return out of it. 
+    // Your code now disables the button and can only be enabled on refresh.
+    // Also read your Questions properly. You were meant to use alert not prompt.
+
     if (usernameIsValid && passwordIsValid) {
       console.log("valid");
       prompt("Signed in!");
